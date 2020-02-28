4c2f5d983ac04f9a70ef1c5c7a0cf78ee62afc57
Darrrah <darashalabi@gmail.com>
2020-02-28 21:17:16 +0200

    -m 'details fixed'

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-'use strict'
+// 'use strict'
+// var strikethrough= document.getElementById("strike");
+// strikethrough.addEventListener('click' , struckCheckbox);
+
+// function struckCheckbox(){
+// strikethrough.style.setProperty('text-decoration' , 'line-through');
+// };
